2b1084f38cfb01db314278d7847077ee69fa558d
Aldre <anderlouedomniques@gmail.com>
2022-10-14 18:28:24 -0300

    fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,22 +1,12 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { GlobalProvider } from 'context/GlobalContext'
-import { GlobalStyleRender } from './style'
-import { StyleTest } from './style'
-import { Theme } from 'styles/theme'
 
 function MyApp({ Component, pageProps }: AppProps) {
 
   return (
     <GlobalProvider>
-      {/* <GlobalStyleRender> */}
-      {/* <> */}
-        {/* <StyleTest theme={theme}> */}
-          <Component {...pageProps} />
-
-        {/* </StyleTest> */}
-      {/* </> */}
-      {/* </GlobalStyleRender> */}
+      <Component {...pageProps} />
     </GlobalProvider>
 
   )
